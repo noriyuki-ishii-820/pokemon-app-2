ecc543977bf0aacdf3d0778c45643c915ea84363
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2021-05-05 16:30:51 +1000

    navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom' 
 import NavbarComponent from "./components/Navbar/Navbar"
 import Home from "./pages/Home/Home"
+import Search from "./pages/Search/Search"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
     return (
@@ -13,6 +13,7 @@ function App() {
             <NavbarComponent />
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/search" exact component={Search} />
             </Switch>
         </Router>
             
