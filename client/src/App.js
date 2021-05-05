6049bb1fcbd675aaacff0727cf7e30b55f92099f
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2021-05-05 17:00:48 +1000

    search working

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import NavbarComponent from "./components/Navbar/Navbar"
 import Home from "./pages/Home/Home"
 import Search from "./pages/Search/Search"
+import Team from "./pages/Team/Team"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +15,8 @@ function App() {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/search" exact component={Search} />
+                <Route path="/team" exact component={Team} />
+
             </Switch>
         </Router>
             
--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import validator from "validator";
 
 function Search() {
-    return (
-        <div>
-            <h1>this is the search page</h1>
-        </div>
-    )
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+  const URL = "https://pokeapi.co/api/v2/pokemon?limit=2000";
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const response = await fetch(URL);
+      const jsonData = await response.json();
+      setData(jsonData.results);
+    };
+    getPokemon();
+  }, []);
+
+  const SearchInput = (e) => {
+    e.preventDefault();
+    var input = document.getElementById("input").value.trim();
+
+    if (
+      validator.isInt(input, {
+        allow_leading_zeroes: false,
+        min: 1,
+        max: data.length,
+      })
+    ) {
+      console.log("it's a valid number!", input);
+    } else if (validator.isAlpha(input)) {
+      console.log("it's a word!");
+    } else {
+      console.log("to be validated");
+    }
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setInput(e.target.value);
+}
+  return (
+    <div>
+      <form onSubmit={SearchInput}>
+        <input 
+            id="input" 
+            list="data"
+            onChange={handleChange} />
+
+        
+        <datalist id="data">
+          {data.map((each, i) => (
+            <option
+              value={each.name.charAt(0).toUpperCase() + each.name.slice(1)}
+              key={i}
+            ></option>
+          ))}
+        </datalist>
+        <button variant="contained" id="button" color="primary" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
